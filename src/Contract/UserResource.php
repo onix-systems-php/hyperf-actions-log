39383f1074cbc33c0cd8c9faef640e80624f8d7b
Denys Bobrovnytskyi <denys.bobrovnytskyi@onix-systems.com>
2023-02-02 16:27:44 +0200

    fix(dependencies): UserResource

---
+++ src/Contract/UserResource.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace OnixSystemsPHP\HyperfActionsLog\Contract;
+
+use OnixSystemsPHP\HyperfCore\Resource\AbstractResource;
+
+class UserResource extends AbstractResource
+{
+}
