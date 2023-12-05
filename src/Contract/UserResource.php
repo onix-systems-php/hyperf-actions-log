cea7e71b96ed92209462b8d33efae1b88894ebf6
Alex Syvolap <80973788+alexsyvolap@users.noreply.github.com>
2023-12-05 17:11:04 +0200

    hyperf-3.1: Update code with detailed comments and improve code style

--- src/Contract/UserResource.php
+++ src/Contract/UserResource.php
@@ -1,10 +1,17 @@
 <?php
+
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 
 namespace OnixSystemsPHP\HyperfActionsLog\Contract;
 
 use OnixSystemsPHP\HyperfCore\Resource\AbstractResource;
 
-class UserResource extends AbstractResource
-{
-}
+class UserResource extends AbstractResource {}
--- src/Service/ActionsListingService.php
+++ src/Service/ActionsListingService.php
@@ -1,6 +1,15 @@
 <?php
 
 declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
 namespace OnixSystemsPHP\HyperfActionsLog\Service;
 
 use OnixSystemsPHP\HyperfActionsLog\Repository\ActionRepository;
@@ -12,13 +21,10 @@ use OnixSystemsPHP\HyperfCore\Service\Service;
 #[Service]
 class ActionsListingService
 {
-
     public function __construct(
         private ActionRepository $rActionLog,
         private ?CorePolicyGuard $policyGuard,
-    ) {
-    }
-
+    ) {}
 
     public function list(array $filters, PaginationRequestDTO $paginationRequest): PaginationResultDTO
     {
